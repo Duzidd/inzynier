b8c35cb8220877e908a308101a9b769eac8e3673
Jakub Stachowicz <kuba2000s1@gmail.com>
2024-01-24 23:37:38 +0100

    Przyciski są okrągłe i gridy prawie ładne

---
+++ Views/EditProductWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+
+
+namespace inzynier.Views
+{
+    public partial class EditProductWindow : Window
+    {
+        private int productId;
+
+        public EditProductWindow(int productId, string name, int hight, int width, string warehouse, string location)
+        {
+            InitializeComponent();
+
+            // Wypełnij pola tekstowe danymi zaznaczonego produktu
+            this.productId = productId;
+            txtName.Text = name;
+            txtHight.Text = hight.ToString();
+            txtWidth.Text = width.ToString();
+            txtWarehouse.Text = warehouse;
+            txtLocation.Text = location;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtHight.Text, out int newHight) && int.TryParse(txtWidth.Text, out int newWidth))
+            {
+                string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+                // Sprawdź, czy magazyn istnieje w tabeli Warehouse
+                if (!WarehouseExists(txtWarehouse.Text))
+                {
+                    MessageBox.Show("Podany magazyn nie istnieje.");
+                    return;
+                }
+
+                // Sprawdź, czy lokalizacja istnieje w tabeli Warehouse
+                if (!LocationExists(txtLocation.Text))
+                {
+                    MessageBox.Show("Podana lokalizacja nie istnieje.");
+                    return;
+                }
+
+                using SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string updateSql = $"UPDATE [inz_xd].[dbo].[Products_inz] SET [Name] = '{txtName.Text}', [Hight] = {newHight}, [Width] = {newWidth}, [Warehouse] = '{txtWarehouse.Text}', [Location] = '{txtLocation.Text}' WHERE [Id] = {productId}";
+
+                using SqlCommand command = new SqlCommand(updateSql, connection);
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Zaktualizowano dane produktu.");
+                Close(); // Zamyka okno po zatwierdzeniu edycji
+            }
+            else
+            {
+                MessageBox.Show("Wprowadź poprawne dane.");
+            }
+        }
+
+        private bool WarehouseExists(string warehouse)
+        {
+            // Sprawdź, czy magazyn istnieje w tabeli Warehouse
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string checkWarehouseSql = $"SELECT COUNT(*) FROM [inz_xd].[dbo].[Warehouse] WHERE [Warehouse] = '{warehouse}'";
+
+            using SqlCommand checkWarehouseCommand = new SqlCommand(checkWarehouseSql, connection);
+            int count = (int)checkWarehouseCommand.ExecuteScalar();
+
+            return count > 0;
+        }
+
+        private bool LocationExists(string location)
+        {
+            // Sprawdź, czy lokalizacja istnieje w tabeli Warehouse
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            string checkLocationSql = $"SELECT COUNT(*) FROM [inz_xd].[dbo].[Warehouse] WHERE [Location] = '{location}'";
+
+            using SqlCommand checkLocationCommand = new SqlCommand(checkLocationSql, connection);
+            int count = (int)checkLocationCommand.ExecuteScalar();
+
+            return count > 0;
+        }
+
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Zamknij okno bez zapisywania zmian
+            Close();
+        }
+    }
+}
--- Views/MoveProduct.xaml.cs
+++ Views/MoveProduct.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using inzynier.Temp;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,10 +11,37 @@ namespace inzynier.Views
     /// </summary>
     public partial class MoveProduct : Window
     {
+        
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Tutaj umieść kod, który chcesz wykonać przy inicjalizacji okna
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+            string sql = "SELECT  [Id],[Name],[Hight]   ,[Width],[Warehouse]   ,[Location]   FROM [inz_xd].[dbo].[Products_inz]";
+
+            // Tworzenie obiektu SqlDataAdapter
+            SqlDataAdapter adapter = new(sql, connection);
+
+            // Tworzenie obiektu DataTable
+            DataTable dataTable = new();
+
+            // Wypełnianie DataTable danymi z bazy danych
+            adapter.Fill(dataTable);
+
+            // Przypisanie DataTable do DataGrida
+            gri.ItemsSource = dataTable.DefaultView;
+        }
         public MoveProduct()
         {
             InitializeComponent();
+            Loaded += Window_Loaded;
         }
+
+
         private void Windows_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -60,5 +89,60 @@ namespace inzynier.Views
                 this.Close();
             }
         }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // Pobierz zaznaczony wiersz z DataGrid
+            DataRowView selectedRow = (DataRowView)gri.SelectedItem;
+
+            if (selectedRow != null)
+            {
+                // Pobierz dane z zaznaczonego rekordu
+                int productId = (int)selectedRow["Id"];
+                string name = selectedRow["Name"].ToString();
+
+                // Sprawdź, czy dane są odpowiedniego typu
+                int hight;
+                int width;
+                if (int.TryParse(selectedRow["Hight"].ToString(), out hight) &&
+                    int.TryParse(selectedRow["Width"].ToString(), out width))
+                {
+                    string warehouse = selectedRow["Warehouse"].ToString();
+                    string location = selectedRow["Location"].ToString();
+
+                    // Utwórz i otwórz okno edycji
+                    EditProductWindow editWindow = new EditProductWindow(productId, name, hight, width, warehouse, location);
+                    editWindow.ShowDialog();
+
+                    // Aktualizuj dane w DataGrid po edycji
+                    RefreshData();
+                }
+                else
+                {
+                    MessageBox.Show("Nieprawidłowe dane w kolumnach 'Hight' lub 'Width'.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Proszę wybrać wiersz do edycji.");
+            }
+        }
+
+        private void RefreshData()
+        {
+            // Refresh the DataGrid with updated data
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+            string sql = "SELECT  [Id],[Name],[Hight]   ,[Width],[Warehouse]   ,[Location]   FROM [inz_xd].[dbo].[Products_inz]";
+
+            SqlDataAdapter adapter = new(sql, connection);
+            DataTable dataTable = new();
+            adapter.Fill(dataTable);
+
+            gri.ItemsSource = dataTable.DefaultView;
+        }
+
     }
 }
